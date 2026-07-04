d596fadcb3cf193f511aa124a924ed9d4f266cd4
Axword <xAxwordx@gmail.com>
2026-07-04 23:47:32 +0200

    fix: hostname

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ class ArenaIO {
     }
     async initNetwork() {
         try {
-            await this.wsClient.connect("wss://vamps-io.onrender.com");
+            await this.wsClient.connect("wss://vamp-io.onrender.com");
             this.isOnline = true;
 
             console.log('[Network] Connected - multiplayer available');
